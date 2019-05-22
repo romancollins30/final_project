acb606b212edc497f5e1e619b5995dc44185c709
erimerza <erimerza@gmail.com>
2019-05-21 23:09:19 -0700

    cat model added to page

--- DoesNotExist/app.py
+++ DoesNotExist/app.py
@@ -74,5 +74,58 @@ def index():
     return render_template("index.html")
 
 
+
+@app.route('/cats', methods=['GET', 'POST'])
+def cat_upload_file():
+    #upload
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename = "uploadedfile.png"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #test & results
+            filename = "./models/cat_model"
+            model = pickle.load(open(filename, "rb"))
+            path = "testing/"
+            filelist = os.listdir(path)
+            for x in filelist:
+                if x.endswith(".png"):
+                    img = kp.image.load_img(path+x, target_size=(227, 227))
+                    y = kp.image.img_to_array(img)
+                    y = y.transpose(2,0,1).reshape(1,-1)
+                    guess = model.predict(y)
+                    if guess[0] == 1:
+                        print('real')
+                        return render_template("catsr.html")
+                    else:
+                        print('fake')
+                        return render_template("catsf.html")
+                if x.endswith(".PNG"):
+                    img = kp.image.load_img(path+x, target_size=(227, 227))
+                    y = kp.image.img_to_array(img)
+                    y = y.transpose(2,0,1).reshape(1,-1)
+                    if guess[0] == 1:
+                        print('real')
+                        return render_template("catsr.html")
+                    else:
+                        print('fake')
+                        return render_template("catsf.html")
+    return render_template("cats.html")
+
+@app.route('/cats')
+def cats():
+    return render_template("cats.html")
+
+
 if __name__ == '__main__':
    app.run(debug = True)
